498daf2355525b40d6a70e0f130808edc90b213e
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-02-18 14:16:22 +0200

    Add files via upload

---
+++ Python OOP/Python OOP - Exams/15Python OOP Exam - 15 August 2021 - DONE (2hours and 20min)/project/baked_food/cake.py
@@ -0,0 +1,9 @@
+from project.baked_food.baked_food import BakedFood
+
+
+class Cake(BakedFood):
+    cake_portion = 245
+
+    def __init__(self, name: str, price: float):
+        super().__init__(name, self.cake_portion, price)
+
