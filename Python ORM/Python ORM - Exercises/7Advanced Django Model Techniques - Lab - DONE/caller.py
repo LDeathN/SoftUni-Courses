eab617d7642ae425ab7b7d6702bc446367ae99fd
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-06 16:09:04 +0200

    Add files via upload

---
+++ Python ORM/Python ORM - Exercises/7Advanced Django Model Techniques - Lab - DONE/caller.py
@@ -0,0 +1,12 @@
+import os
+import django
+
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models
+# Create and check models
+# Run and print your queries
+# Create a Product instance
