2e29a943c9c53f8a46aba1e1667766da99192ce3
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-15 15:04:06 +0200

    Update 1Data Types and Table Basics - Exercise.py

--- PostgreSQL/PostgreSQL - Exercises/1Data Types and Table Basics - Exercise.py
+++ PostgreSQL/PostgreSQL - Exercises/1Data Types and Table Basics - Exercise.py
@@ -1,9 +1,17 @@
 #First Problem
-CREATE TABLE minions(
+CREATE TABLE minions_info(
 id SERIAL PRIMARY KEY,
 name VARCHAR(30),
 age INTEGER
 );
 
 
-#
+#Second Problem
+ALTER TABLE minions_info
+ADD code CHAR(4);
+
+ALTER TABLE minions_info
+ADD task TEXT;
+
+ALTER TABLE minions_info
+ADD salary NUMERIC(8, 3);
