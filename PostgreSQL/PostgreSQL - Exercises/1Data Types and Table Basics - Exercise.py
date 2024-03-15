68be1aae55c948a77d8147c18250468306f09b3a
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-15 15:05:07 +0200

    Update 1Data Types and Table Basics - Exercise.py

--- PostgreSQL/PostgreSQL - Exercises/1Data Types and Table Basics - Exercise.py
+++ PostgreSQL/PostgreSQL - Exercises/1Data Types and Table Basics - Exercise.py
@@ -31,3 +31,14 @@ ALTER TABLE minions_info
 ALTER COLUMN equipped SET NOT NULL;
 
 
+#Fifth Problem
+CREATE TYPE type_mood AS ENUM (
+'happy',
+'relaxed',
+'stressed',
+'sad');
+
+ALTER TABLE minions_info
+ADD COLUMN mood type_mood;
+
+
