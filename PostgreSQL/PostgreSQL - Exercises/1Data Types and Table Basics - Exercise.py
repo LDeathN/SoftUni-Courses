2b250474e9f634a4820796eebd16325758c7970a
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-15 15:05:27 +0200

    Update 1Data Types and Table Basics - Exercise.py

--- PostgreSQL/PostgreSQL - Exercises/1Data Types and Table Basics - Exercise.py
+++ PostgreSQL/PostgreSQL - Exercises/1Data Types and Table Basics - Exercise.py
@@ -42,3 +42,10 @@ ALTER TABLE minions_info
 ADD COLUMN mood type_mood;
 
 
+#Sixth Problem
+ALTER TABLE minions_info
+ALTER COLUMN age SET DEFAULT 0,
+ALTER COLUMN name SET DEFAULT '',
+ALTER COLUMN code SET DEFAULT '';
+
+
