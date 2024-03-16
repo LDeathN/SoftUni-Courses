fd26c29c8a08eddd79573a4b0aaa9824c6a91f5b
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-16 15:00:25 +0200

    Update 2Basic CRUD - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/2Basic CRUD - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/2Basic CRUD - Lab.py
@@ -40,3 +40,12 @@ SELECT * FROM employees
 ORDER BY id;
 
 
+#Seventh Problem
+CREATE VIEW top_paid_employee_view AS
+SELECT *
+FROM employees
+ORDER BY salary DESC
+LIMIT 1;
+
+SELECT *
+FROM top_paid_employee_view;
