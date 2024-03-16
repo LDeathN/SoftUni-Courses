5c59ea307f321192ff7759971aaced4e325321da
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-16 14:59:08 +0200

    Update 2Basic CRUD - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/2Basic CRUD - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/2Basic CRUD - Lab.py
@@ -3,3 +3,8 @@ SELECT id, CONCAT(first_name, ' ', last_name) AS "Full Name", job_title AS "Job 
 
 
 #Second Problem
+SELECT id, CONCAT(first_name, ' ', last_name) AS "full_name", job_title, salary FROM employees
+WHERE salary > 1000.00
+ORDER BY id;
+
+
