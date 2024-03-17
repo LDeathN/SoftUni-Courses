3cf7689ece34d0ebfb440060b14fe4fab7afd1c4
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-17 11:08:57 +0200

    Update 2Basic CRUD - Exercise.py

--- PostgreSQL/PostgreSQL - Exercises/2Basic CRUD - Exercise.py
+++ PostgreSQL/PostgreSQL - Exercises/2Basic CRUD - Exercise.py
@@ -43,3 +43,37 @@ OR number < 1000;
 SELECT employee_id, project_id FROM employees_projects
 WHERE employee_id IN (200, 250)
 AND project_id NOT IN (50, 100);
+
+
+SELECT name, start_date FROM projects
+WHERE name IN ('Mountain', 'Road', 'Touring')
+LIMIT 20;
+
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name, job_title, salary FROM employees
+WHERE salary = 12500
+OR salary = 14000
+OR salary = 23600
+OR salary = 25000
+ORDER BY salary DESC;
+
+
+SELECT id, first_name, last_name FROM employees
+WHERE middle_name IS NULL
+LIMIT 3;
+
+
+INSERT INTO departments(department, manager_id)
+VALUES
+('Finance', 3),
+('Information Services', 42),
+('Document Control', 90),
+('Quality Assurance', 274),
+('Facilities and Maintenance', 218),
+('Shipping and Receiving', 85),
+('Executive', 109);
+
+
+CREATE TABLE company_chart AS
+SELECT CONCAT(first_name, ' ', last_name) AS full_name, job_title, department_id, manager_id
+FROM employees;
