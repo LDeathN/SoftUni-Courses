86c16992bf8124eea448de63866df5c67f673000
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-17 11:08:13 +0200

    Update 2Basic CRUD - Exercise.py

--- PostgreSQL/PostgreSQL - Exercises/2Basic CRUD - Exercise.py
+++ PostgreSQL/PostgreSQL - Exercises/2Basic CRUD - Exercise.py
@@ -18,3 +18,28 @@ LIMIT 50;
 SELECT id AS id, CONCAT(first_name, ' ', middle_name, ' ', last_name) AS full_name, hire_date FROM employees
 ORDER BY hire_date 
 OFFSET 9 ROWS;
+
+
+SELECT id, CONCAT(number, ' ', street) AS address, city_id FROM addresses
+WHERE id >= 20;
+
+
+SELECT CONCAT(number, ' ', street) AS address, city_id FROM addresses
+WHERE city_id % 2 = 0 AND city_id > 0
+ORDER BY city_id;
+
+
+SELECT name, start_date, end_date FROM projects
+WHERE start_date >= '2016-06-01 07:00:00'
+AND end_date < '2023-06-04 00:00:00'
+ORDER BY start_date;
+
+
+SELECT number, street FROM addresses
+WHERE id BETWEEN 50 AND 100
+OR number < 1000;
+
+
+SELECT employee_id, project_id FROM employees_projects
+WHERE employee_id IN (200, 250)
+AND project_id NOT IN (50, 100);
