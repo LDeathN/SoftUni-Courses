c2baa31f29bcd8c386833d519944dec837b0ab08
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-20 14:16:20 +0200

    Update 4Data Aggregation - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
@@ -6,3 +6,11 @@ GROUP BY department_id
 ORDER BY department_id;
 
 
+#Second Problem
+SELECT "department_id",
+COUNT("salary") AS "employee_count"
+FROM "employees"
+GROUP BY "department_id"
+ORDER BY "department_id";
+
+
