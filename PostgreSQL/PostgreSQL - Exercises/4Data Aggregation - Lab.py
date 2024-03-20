fb0db9fb966637ba2aaa0672e6d497796f188a7f
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-20 14:18:06 +0200

    Update 4Data Aggregation - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
@@ -55,3 +55,14 @@ HAVING SUM(salary) < 4200
 ORDER BY "department_id";
 
 
+#Eight Problem
+SELECT id, first_name, last_name,
+TRUNC(salary, 2) AS salary,
+department_id,
+CASE department_id
+WHEN 1 THEN 'Management'
+WHEN 2 THEN 'Kitchen Staff'
+WHEN 3 THEN 'Service Staff'
+ELSE 'Other'
+END AS department_name
+FROM employees ORDER BY id;
