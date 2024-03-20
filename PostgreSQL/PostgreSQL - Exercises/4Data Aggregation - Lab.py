7407d9f4fe50bfaa6e4b1bde5779e2bb32dfc9fb
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-20 14:16:46 +0200

    Update 4Data Aggregation - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
@@ -22,3 +22,11 @@ GROUP BY "department_id"
 ORDER BY "department_id";
 
 
+#Fourth Problem
+SELECT "department_id",
+MAX(salary) AS "max_salary"
+FROM "employees"
+GROUP BY "department_id"
+ORDER BY "department_id";
+
+
