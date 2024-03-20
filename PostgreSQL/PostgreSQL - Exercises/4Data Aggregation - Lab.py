761b94d70834dd3da76d628fb8b67348c8d40616
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-20 14:17:08 +0200

    Update 4Data Aggregation - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
@@ -30,3 +30,11 @@ GROUP BY "department_id"
 ORDER BY "department_id";
 
 
+#Fifth Problem
+SELECT "department_id",
+MIN(salary) AS "min_salary"
+FROM "employees"
+GROUP BY "department_id"
+ORDER BY "department_id";
+
+
