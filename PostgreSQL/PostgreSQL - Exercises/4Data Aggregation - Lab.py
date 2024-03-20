8c7b32d4a2a62951792584ca2fffcff55db29414
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-20 14:17:22 +0200

    Update 4Data Aggregation - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Lab.py
@@ -38,3 +38,11 @@ GROUP BY "department_id"
 ORDER BY "department_id";
 
 
+#Sixth Problem
+SELECT "department_id",
+AVG(salary) AS "avg_salary"
+FROM "employees"
+GROUP BY "department_id"
+ORDER BY "department_id";
+
+
