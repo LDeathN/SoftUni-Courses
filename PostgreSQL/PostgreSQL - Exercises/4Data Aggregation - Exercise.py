6d77e2e415e054e410efee14f9390cd65a282adc
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-21 16:25:36 +0200

    Update 4Data Aggregation - Exercise.py

--- PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Exercise.py
+++ PostgreSQL/PostgreSQL - Exercises/4Data Aggregation - Exercise.py
@@ -14,3 +14,36 @@ SELECT MIN(deposit_charge) AS minimum_deposit_charge
 FROM wizard_deposits;
 
 
+SELECT MAX(age) AS maximum_age
+FROM wizard_deposits;
+
+
+SELECT deposit_group, 
+SUM(deposit_interest) AS deposit_interest
+FROM wizard_deposits
+GROUP BY deposit_group
+ORDER BY deposit_interest DESC;
+
+
+SELECT magic_wand_creator,
+MIN(magic_wand_size) AS minimum_wand_size
+FROM wizard_deposits
+GROUP BY magic_wand_creator
+ORDER BY minimum_wand_size
+LIMIT 5;
+
+
+SELECT
+  deposit_group,
+  is_deposit_expired,
+  FLOOR(AVG(deposit_interest)) AS deposit_interest
+FROM
+  wizard_deposits
+WHERE
+  deposit_start_date > '1985-01-01'
+GROUP BY
+  deposit_group, is_deposit_expired
+ORDER BY
+  deposit_group DESC, is_deposit_expired ASC;
+
+
