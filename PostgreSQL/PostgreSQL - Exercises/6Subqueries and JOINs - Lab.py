69af5f09f4d0b4b70b5adcb9922aa1c0d10da201
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-24 10:52:30 +0200

    Update 6Subqueries and JOINs - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/6Subqueries and JOINs - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/6Subqueries and JOINs - Lab.py
@@ -9,3 +9,13 @@ OR t.name LIKE '%Carnation%'
 ORDER BY a.town_id, a.address_id;
 
 
+#Second Problem
+SELECT e.employee_id, CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+d.department_id, d.name AS department_name
+FROM employees AS e
+JOIN departments AS d
+ON e.employee_id = d.manager_id
+ORDER BY e.employee_id
+LIMIT 5;
+
+
