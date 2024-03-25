7eb3f28cad802363ad6c65d6f1b288b685d5a2ed
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-25 18:39:39 +0200

    Delete CalisthenicsWorkoutTracker/exercises directory

--- CalisthenicsWorkoutTracker/exercises/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from CalisthenicsWorkoutTracker.exercises.models import Exercise
-
-
-@admin.register(Exercise)
-class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'difficulty', 'repetitions',)
-
