1afb03697560b59415a4ab0592c0b16247364f6c
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-25 18:39:34 +0200

    Delete CalisthenicsWorkoutTracker/meal_plans directory

--- CalisthenicsWorkoutTracker/meal_plans/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from CalisthenicsWorkoutTracker.meal_plans.models import MealPlan
-
-
-@admin.register(MealPlan)
-class MealPlanAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'meal_plan_type', 'goal_calories')
-
