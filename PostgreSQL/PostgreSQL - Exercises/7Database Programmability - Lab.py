a6b8fdb423f0f19b1dc86a714ff64fda3ba65bf1
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-29 11:27:01 +0200

    Update 7Database Programmability - Lab.py

--- PostgreSQL/PostgreSQL - Exercises/7Database Programmability - Lab.py
+++ PostgreSQL/PostgreSQL - Exercises/7Database Programmability - Lab.py
@@ -14,3 +14,15 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+#Second Problem
+CREATE PROCEDURE sp_increase_salaries(department_name varchar(50))
+LANGUAGE plpgsql
+AS $$
+BEGIN
+UPDATE employees AS e
+SET salary = salary * 1.05
+WHERE e.department_id = (
+SELECT department_id FROM departments WHERE name = department_name);
+END; $$;
+
+
