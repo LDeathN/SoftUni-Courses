e8ea3be8b0c6bb73182588bd035ac5bebfdda857
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-03-30 15:00:40 +0200

    Update 7Functions and Procedures - Exercise.py

--- PostgreSQL/PostgreSQL - Exercises/7Functions and Procedures - Exercise.py
+++ PostgreSQL/PostgreSQL - Exercises/7Functions and Procedures - Exercise.py
@@ -56,3 +56,62 @@ BEGIN
     RETURN TRUE;
 END;
 $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION fn_is_game_over(
+    is_game_over BOOLEAN
+)
+RETURNS TABLE (
+    name VARCHAR(50),
+    game_type_id INT,
+    is_finished BOOLEAN
+) AS $$
+BEGIN
+    RETURN QUERY 
+    SELECT g.name, g.game_type_id, g.is_finished
+    FROM games AS g
+    WHERE g.is_finished = is_game_over;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION fn_difficulty_level(level INT)
+RETURNS VARCHAR(50) AS $$
+BEGIN
+    IF level <= 40 THEN
+        RETURN 'Normal Difficulty';
+    ELSIF level BETWEEN 41 AND 60 THEN
+        RETURN 'Nightmare Difficulty';
+    ELSE
+        RETURN 'Hell Difficulty';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT
+    user_id,
+    level,
+    cash,
+    fn_difficulty_level(level) AS difficulty_level
+FROM
+    users_games
+ORDER BY
+    user_id ASC;
+
+
+CREATE OR REPLACE FUNCTION fn_cash_in_users_games(game_name VARCHAR(50))
+RETURNS TABLE (total_cash NUMERIC) AS $$
+BEGIN
+    RETURN QUERY 
+    WITH ranked_games AS (
+	SELECT cash, ROW_NUMBER() OVER (ORDER BY cash DESC) AS row_num
+	FROM users_games AS ug
+	JOIN games AS g ON ug.game_id = g.id
+	WHERE g.name = game_name
+	)
+	
+	SELECT ROUND(SUM(cash), 2) AS total_cash
+	FROM ranked_games
+	WHERE row_num % 2 <> 0;
+END;
+$$ LANGUAGE plpgsql;
