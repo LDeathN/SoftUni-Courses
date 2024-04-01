3714bb1d3c5d134a3022c17a0c964cb5a41016f6
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-04-01 13:26:05 +0300

    Update 3Exam Preparation II.py

--- PostgreSQL/PostgreSQL - Exams/3Exam Preparation II.py
+++ PostgreSQL/PostgreSQL - Exams/3Exam Preparation II.py
@@ -160,3 +160,84 @@ JOIN categories ca ON c.category_id = ca.id
 ORDER BY co.id;
 
 
+CREATE FUNCTION fn_courses_by_client(
+phone_num VARCHAR(20)
+)
+RETURNS INT
+AS $$
+DECLARE
+	count_value INT;
+BEGIN
+	SELECT COUNT(co.client_id) INTO count_value
+	FROM clients c
+	JOIN courses co ON c.id = co.client_id
+	WHERE c.phone_number = phone_num;
+	
+	RETURN count_value;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE TABLE search_results (
+    id SERIAL PRIMARY KEY,
+    address_name VARCHAR(50),
+    full_name VARCHAR(100),
+    level_of_bill VARCHAR(20),
+    make VARCHAR(30),
+    condition CHAR(1),
+    category_name VARCHAR(50)
+);
+
+CREATE OR REPLACE PROCEDURE sp_courses_by_address(
+IN address_name VARCHAR(100))
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	result_record RECORD;
+BEGIN
+	TRUNCATE TABLE search_results;
+	INSERT INTO search_results(
+	address_name,
+	full_name,
+	level_of_bill,
+	make,
+	"condition",
+	category_name)
+	SELECT a.name AS address_name,
+	c.full_name,
+	CASE
+		WHEN co.bill <= 20 THEN 'Low'
+		WHEN co.bill <= 30 THEN 'Medium'
+		ELSE 'High'
+	END AS level_of_bill,
+	ca.make,
+	ca.condition,
+	cat.name AS category_name
+	FROM courses co
+	INNER JOIN addresses a ON co.from_address_id = a.id
+	INNER JOIN clients c ON co.client_id = c.id
+	INNER JOIN cars ca ON co.car_id = ca.id
+	INNER JOIN categories cat ON ca.category_id = cat.id
+	WHERE a.name = address_name
+	ORDER BY ca.make, c.full_name;
+	
+	FOR result_record
+	IN SELECT *
+	FROM search_results
+	LOOP RAISE NOTICE
+	'Address: %,
+	Full Name: %,
+	Level of Bill; %,
+	Make: %,
+	Condition: %,
+	Category: %',
+	result_record.address_name,
+	result_record.full_name,
+	result_record.level_of_bill,
+	result_record.make,
+	result_record.condition,
+	result_record.category_name;
+	END LOOP;
+END;
+$$;
