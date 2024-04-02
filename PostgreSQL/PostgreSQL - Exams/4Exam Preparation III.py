93c025c1a0fc1544f986f680efffeff8a82933ae
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-04-02 14:27:13 +0300

    Update 4Exam Preparation III.py

--- PostgreSQL/PostgreSQL - Exams/4Exam Preparation III.py
+++ PostgreSQL/PostgreSQL - Exams/4Exam Preparation III.py
@@ -180,3 +180,80 @@ GROUP BY c.last_name, p.name
 ORDER BY average_rating DESC;
 
 
+CREATE OR REPLACE FUNCTION fn_creator_with_board_games(
+"name" VARCHAR(30)
+)
+RETURNS INT
+AS $$
+DECLARE
+	count_value INT;
+BEGIN
+	SELECT(COUNT(cbg.creator_id) INTO count_value)
+	FROM creators_board_games cbg
+	JOIN creators c ON cbg.creator_id = c.id
+	WHERE c.first_name = "name";
+	
+	RETURN count_value;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE TABLE search_results (
+	"id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50),
+    release_year INT,
+    rating FLOAT,
+    category_name VARCHAR(50),
+    publisher_name VARCHAR(50),
+    min_players VARCHAR(50),
+    max_players VARCHAR(50)
+);
+
+CREATE OR REPLACE PROCEDURE usp_search_by_category(
+	IN category VARCHAR(50)
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    TRUNCATE TABLE search_results;
+    
+    INSERT INTO search_results (
+		"name", 
+		release_year, 
+		rating, 
+		category_name, 
+		publisher_name, 
+		min_players, 
+		max_players
+	)
+    SELECT 
+        bg."name",
+        bg.release_year,
+        bg.rating,
+        c."name" AS category_name,
+        p."name" AS publisher_name,
+        CONCAT(pr.min_players, ' people'),
+        CONCAT(pr.max_players, ' people')
+    FROM 
+		board_games AS bg
+    JOIN 
+		publishers AS p 
+		ON 
+		bg.publisher_id = p.id
+    JOIN 
+		categories AS c 
+		ON 
+		bg.category_id = c."id"
+    JOIN 
+		players_ranges AS pr 
+		ON 
+		bg.players_range_id = pr."id"
+    WHERE 
+		c."name" = category
+    ORDER BY 
+		publisher_name, 
+		release_year DESC;
+END;
+$$;
+
