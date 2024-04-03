207c159d32b892ce022b5cf15124f2a40b0b4871
Martin Daskalov <140381179+LDeathN@users.noreply.github.com>
2024-04-03 09:13:45 +0300

    Update 1PostgreSQL Exam - 14 October 2023.py

--- PostgreSQL/PostgreSQL - Exams/1PostgreSQL Exam - 14 October 2023.py
+++ PostgreSQL/PostgreSQL - Exams/1PostgreSQL Exam - 14 October 2023.py
@@ -96,3 +96,27 @@ CREATE TABLE players_coaches(
 );
 
 
+INSERT INTO coaches(first_name, last_name, salary, coach_level)
+SELECT first_name, last_name, salary * 2 AS salary, LENGTH(first_name) AS coach_level
+FROM players
+
+
+UPDATE coaches
+SET salary = salary * coach_level
+WHERE first_name LIKE 'C%' AND id IN (
+    SELECT DISTINCT coach_id
+    FROM players_coaches
+    WHERE coach_id IS NOT NULL
+);
+
+
+DELETE FROM players_coaches
+WHERE player_id IN (
+SELECT id FROM players
+WHERE hire_date < '2013-12-13 07:18:46'
+);
+
+DELETE FROM players
+WHERE hire_date < '2013-12-13 07:18:46';
+
+
